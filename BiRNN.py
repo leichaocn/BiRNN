246bea0b73ecbec0548663796f764cf8f5f01bd6
Lei Chao <leichaocn@163.com>
2017-11-07 09:47:39 +0800

    增加了region标签
    方便用pycharm打开,理解代码结构

--- BiRNN.py
+++ BiRNN.py
@@ -1,44 +1,22 @@
-#%%
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+# region 载入库,配置参数
 import tensorflow as tf
 import numpy as np
 
-# Import MINST data
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-
-
-
-# Parameters
-learning_rate = 0.01
-max_samples = 400000
+learning_rate = 0.01    # 因为优化器是adam,所以学习速率较低
+max_samples = 400000    # 最大训练样本数为40万
 batch_size = 128
-display_step = 10
+display_step = 10       # 每间隔10次训练,就展示一次训练情况
+n_input = 28 # 图像的宽度为28,因此设置输入为28
+n_steps = 28 # 图像的高度为28,因此设置LSTM的展开步数(unrolled steps of LSTM)也设置为28
+n_hidden = 256 # 定义一个方向的cell的数量
+n_classes = 10 # 0-9,共有10个分类.
 
-# Network Parameters
-n_input = 28 # MNIST data input (img shape: 28*28)
-n_steps = 28 # timesteps
-n_hidden = 256 # hidden layer num of features
-n_classes = 10 # MNIST total classes (0-9 digits)
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+# endregion
 
-# tf Graph input
-x = tf.placeholder("float", [None, n_steps, n_input])
-y = tf.placeholder("float", [None, n_classes])
-
-# Define weights
+# region 定义softmax层的权重
+# 因为要合成两个LSTM的输入,所以第一个维度是2*n_hidden
 weights = {
     # Hidden layer weights => 2*n_hidden because of foward + backward cells
     'out': tf.Variable(tf.random_normal([2*n_hidden, n_classes]))
@@ -46,69 +24,45 @@ weights = {
 biases = {
     'out': tf.Variable(tf.random_normal([n_classes]))
 }
+# endregion
 
+# region 构建计算图
+# x是一个二维结构,但是和卷积网络中的空间二维结构不同,
+# 这里的二维被理解成第一个维度是时间序列n_steps,第二维度是每个时间点下的数据n_input
+x = tf.placeholder("float", [None, n_steps, n_input])
+y = tf.placeholder("float", [None, n_classes])
+# 将x拆成一个长度为n_steps的列表,每个元素tensor的尺寸为[batch_size,n_input]
+x_unstack = tf.unstack(x, axis=1)
 
-def BiRNN(x, weights, biases):
+# lstm_fw_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
+lstm_fw_cell = tf.contrib.rnn.GRUCell(n_hidden)
+# lstm_bw_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
+lstm_bw_cell = tf.contrib.rnn.GRUCell(n_hidden)
 
-    # Prepare data shape to match `bidirectional_rnn` function requirements
-    # Current data input shape: (batch_size, n_steps, n_input)
-    # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
-    
-    # Permuting batch_size and n_steps
-    x = tf.transpose(x, [1, 0, 2])
-    # Reshape to (n_steps*batch_size, n_input)
-    x = tf.reshape(x, [-1, n_input])
-    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
-    x = tf.split(x, n_steps)
-
-    # Define lstm cells with tensorflow
-    # Forward direction cell
-    lstm_fw_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-    # Backward direction cell
-    lstm_bw_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-
-    # Get lstm cell output
-#    try:
-    outputs, _, _ = tf.contrib.rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x,
+outputs, _, _ = tf.contrib.rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x_unstack,
                                            dtype=tf.float32)
-#    except Exception: # Old TensorFlow version only returns outputs not states
-#        outputs = rnn.bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x,
-#                                        dtype=tf.float32)
-
-    # Linear activation, using rnn inner loop last output
-    return tf.matmul(outputs[-1], weights['out']) + biases['out']
-    
-
-pred = BiRNN(x, weights, biases)
-
-# Define loss and optimizer
+pred=tf.matmul(outputs[-1], weights['out']) + biases['out']
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-
-# Evaluate model
+# tf.argmax(pred,1)求每行中最大的元素的角标(即列号)
 correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+# tf.cast(correct_pred, tf.float32),将correct_pred转化为浮点型
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-# Initializing the variables
 init = tf.global_variables_initializer()
+# endregion
 
-
-
-# Launch the graph
+# region 执行计算图
 with tf.Session() as sess:
     sess.run(init)
     step = 1
-    # Keep training until reach max iterations
-    while step * batch_size < max_samples:
+    while step * batch_size < max_samples:# step*128<400000
+        # 直接读出来的batch_x的尺寸为[batch_size,784]
         batch_x, batch_y = mnist.train.next_batch(batch_size)
-        # Reshape data to get 28 seq of 28 elements
+        # batch_x经过reshape后,尺寸变为(batch_size, n_steps, n_input)
         batch_x = batch_x.reshape((batch_size, n_steps, n_input))
-        # Run optimization op (backprop)
         sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
         if step % display_step == 0:
-            # Calculate batch accuracy
             acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
-            # Calculate batch loss
             loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
             print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
                   "{:.6f}".format(loss) + ", Training Accuracy= " + \
@@ -116,9 +70,9 @@ with tf.Session() as sess:
         step += 1
     print("Optimization Finished!")
 
-    # Calculate accuracy for 128 mnist test images
     test_len = 10000
     test_data = mnist.test.images[:test_len].reshape((-1, n_steps, n_input))
     test_label = mnist.test.labels[:test_len]
     print("Testing Accuracy:", \
         sess.run(accuracy, feed_dict={x: test_data, y: test_label}))
+# endregion
